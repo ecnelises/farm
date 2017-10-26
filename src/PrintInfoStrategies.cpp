4fe8cb5c5842cd340bccef2b2382182c115bfb18
AndreMiller <305968670@qq.com>
2017-10-26 19:57:37 +0800

    添加注释（调用方法）

--- src/PrintInfoStrategies.cpp
+++ src/PrintInfoStrategies.cpp
@@ -1,5 +1,10 @@
 #include "PrintInfoStrategies.h"
 
+//调用方法
+//PrintInfoStrategy* str = new FirstStrategy();
+//PrintInfo* info = new PrintInfo(str);
+//info->printInfo();
+
 void FirstStrategy::printInfo() {
 	// print info
 
