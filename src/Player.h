4949f80a1d679da214bef559622a47fe064b36ac
DESKTOP-M40RQA1\Wavelee <1322720362@qq.com>
2017-10-26 18:36:12 +0800

    添加了状态类并给player的每个动作加入状态变化，修改了一部分player的变量名

--- src/Player.h
+++ src/Player.h
@@ -5,23 +5,34 @@
 #include "Farm.h"
 #include "Backpack.h"
 #include "BackpackEntry.h"
+#include "State.h"
 
 class Scene;
 class Food;
 
+//enum StateType {
+//	SLEEP, MOVE, EAT, HUNT, GETPACK
+//};
+
 class Player {
 public:
-    Player(double px = 0.0, double py = 0.0);
-    ~Player() = default;
+    Player(double px = 0.0, double py = 0.0, int strength = 100);
+    ~Player();
+	void SetState(State* state);
+	//StateType GetState();
     bool move(const Scene& place);
     void eat(const Food& food);
-    bool kill(const Farm& farm, std::string type);
-    void get(BackpackEntry* backpackEntry);
+	void sleep();
+    bool hunt(const Farm& farm, std::string type);
+    void getpack(BackpackEntry* backpackEntry);
 private:
     int strength;
+	int liveday;
     double x;
     double y;
     Backpack backpack;
+	State *m_state;
+	//StateType state;
 };
 
 #endif
