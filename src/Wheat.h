ce134d100d461353159f362980665ec8dc928d34
CheoGnauhs <tshuang96@gmail.com>
2017-10-26 10:54:53 +0800

    查看小麦状态

--- src/Wheat.h
+++ src/Wheat.h
@@ -6,8 +6,11 @@
 class Wheat : public Crop{
 public:
   virtual ~Wheat();
+  Status getStatus(void) const;
   virtual Food* produce(void) override;
   virtual Corn* propagate(void) const override;
 private:
   Status status;
 }
+
+#endif
