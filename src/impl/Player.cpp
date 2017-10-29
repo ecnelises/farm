df3082acd63602dfae5b39be8fd78d6124a0643c
waveboo <1322720362@qq.com>
2017-10-29 22:26:06 +0800

    Update Player.cpp

--- src/impl/Player.cpp
+++ src/impl/Player.cpp
@@ -5,16 +5,21 @@
 #include "PlayerController.h"
 #include <cmath>
 #include <iostream>
-// #include "SpecificState.h"
+#include "SpecificState.h"
 
 Player::~Player() {
 	delete controller;
 }
 
+void Player::SetState(State *state) {
+	delete m_state;
+	m_state = state;
+}
+
 bool Player::move(const Scene& scene)
 {
-	// SetState(new MoveState());
-	// m_state->Operate(this);
+	 SetState(new MoveState());
+	 m_state->Operate(this);
     /*int d = std::floor(Scene::distance(scene.getX(), scene.getY(), x, y));
     if (strength < d) {
         std::cout << "Move failed.\n";
@@ -32,22 +37,22 @@ bool Player::move(const Scene& scene)
 
 void Player::eat(const Food& food)
 {
-	// SetState(new EatState());
-	// m_state->Operate(this);
+	 SetState(new EatState());
+	 m_state->Operate(this);
     /*strength += food.consume();
     strength = std::min(strength, 100);*/
 }
 
 void Player::sleep() {
-	// SetState(new SleepState());
-	// m_state->Operate(this);
+	 SetState(new SleepState());
+	 m_state->Operate(this);
 }
 
 // ! question:how does user pass the TYPE of Animal to be killed?
 bool Player::hunt(const Farm& farm, std::string type)
 {
-	// SetState(new HuntState());
-	// m_state->Operate(this);
+	 SetState(new HuntState());
+	 m_state->Operate(this);
 //    auto animal = farm.pick(); // don't know how to solve this bug(
 //    auto food = animal->produce();
 //    backpack.push_back(food);
@@ -58,13 +63,15 @@ bool Player::hunt(const Farm& farm, std::string type)
     return true;
 }
 
-void Player::getpack(BackpackEntry* backpackEntry)
-{
-	// SetState(new GetPackState());
-	// m_state->Operate(this);
-    //backpack.push_back(backpackEntry);
+void Player::use(Item* item) {
+    std::cout << "class:Backpack method:use\n";
+    if (item != nullptr) {
+        item->usedBy(this);
+    }
 }
 
-void Player::UseItem(Item* item) {
-	item->getUsedby(this);
+void Player::outputItems(void) {
+    if (controller != nullptr) {
+        controller->getPlayerStatus();
+    }
 }
