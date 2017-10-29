594afd317c119a212f4c43aa4bc04518393c8724
CheoGnauhs <tshuang96@gmail.com>
2017-10-29 23:52:54 +0800

    更改了随机数生成方式

--- src/impl/Map.cpp
+++ src/impl/Map.cpp
@@ -9,10 +9,8 @@
 Map::Map() : player({0, 0}) {
     std::cout << "Map initialized\n";
     player.get().setController(new SimplePlayerController(&player.get()));
-    std::uniform_int_distribution<int> dis(7, 15);
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    int times = dis(gen);
+    srand((unsigned) time(nullptr));
+    int times = rand() % 8 + 7;
     for (int i = 0; i < times; ++i) {
         generateScene();
     }
@@ -24,14 +22,14 @@ Map::~Map() {
     }
 }
 
-void Map::eachScene(SceneStrategy* strategy) {
+void Map::eachScene(SceneStrategy *strategy) {
     std::cout << "class:Map method:eachScene\n";
     for (auto i : scenes) {
         strategy->act(i);
     }
 }
 
-void Map::eachScene(std::function<void(Scene*)> fn) {
+void Map::eachScene(std::function<void(Scene *)> fn) {
     std::cout << "class:Map method:eachScene\n";
     for (auto i : scenes) {
         fn(i);
@@ -40,10 +38,7 @@ void Map::eachScene(std::function<void(Scene*)> fn) {
 
 void Map::generateScene(void) {
     std::cout << "class:Map method:generateScene\n";
-    std::uniform_int_distribution<int> dis(0, 1);
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    if (dis(gen)) {
+    if (rand() % 2) {
         scenes.push_back(new Farm);
     } else {
         scenes.push_back(new Pasture);
