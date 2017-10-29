65f2e1c5d75c5d13a393287cbe2e21710face32c
waveboo <1322720362@qq.com>
2017-10-29 22:24:10 +0800

    Create SpecificState.h

---
+++ src/interface/SpecificState.h
@@ -0,0 +1,50 @@
+#ifndef SPECIFICSTATE_H
+#define SPECIFICSTATE_H
+
+#include"State.h"
+#include <iostream>
+#include"Player.h"
+
+class SleepState : public State{
+public:
+	virtual void Operate(Player* player) {
+		std::cout << "Sleep State"<<std::endl;
+	};
+};
+
+class MoveState : public State {
+public:
+	virtual void Operate(Player* player) {
+		std::cout << "Move State" << std::endl;
+	};
+};
+
+class EatState : public State {
+public:
+	virtual void Operate(Player* player) {
+		std::cout << "Eat State" << std::endl;
+	};
+};
+
+class HuntState : public State {
+public:
+	virtual void Operate(Player* player) {
+		std::cout << "Hunt State" << std::endl;
+	};
+};
+
+class GetPackState : public State {
+public:
+	virtual void Operate(Player* player) {
+		std::cout << "Get Pack State" << std::endl;
+	};
+};
+
+class HangOutState : public State {
+public:
+	virtual void Operate(Player* player) {
+		std::cout << "Hang Out State" << std::endl;
+	};
+};
+
+#endif
