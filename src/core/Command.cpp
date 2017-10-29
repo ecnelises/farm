df7cc4d15d38864dd0970707d6620ecd0a0a6cd8
waveboo <1322720362@qq.com>
2017-10-29 22:26:51 +0800

    Update Command.cpp

--- src/core/Command.cpp
+++ src/core/Command.cpp
@@ -3,7 +3,10 @@
 #include "Scene.h"
 #include "Entity.h"
 #include <iostream>
+#include <iterator>
+#include <cstdlib>
 #include <sstream>
+#include <vector>
 #include <algorithm>
 #include <cctype>
 
@@ -22,6 +25,7 @@ static bool caseInsensitiveCompare(
 }
 
 static Scene* getSceneById(int id) {
+    std::cout << "method:getSceneById id:" << id << '\n';
     Scene* sce = nullptr;
     int cnt = 0;
     Application::instance().getMap().eachScene([&](auto scene){
@@ -39,30 +43,42 @@ CommandInterpreter::CommandInterpreter() {
 Command* CommandInterpreter::interpret(const std::string& cmd) {
     std::cout << "class:CommandInterpreter method:interpret\n";
     std::istringstream iss(cmd);
-    std::string s1;
-    iss >> s1;
-    if (caseInsensitiveCompare(s1, "list")) {
+    std::vector<std::string> words {
+        std::istream_iterator<std::string>{iss},
+        std::istream_iterator<std::string>{}
+    };
+    if (caseInsensitiveCompare(words[0], "list")) {
         return new ListCommand;
-    } else if (caseInsensitiveCompare(s1, "show")) {
-        int s2;
-        iss >> s2;
-        auto scene = getSceneById(s2);
+    } else if (caseInsensitiveCompare(words[0], "show")) {
+        auto scene = getSceneById(std::atoi(words[1].c_str()));
         if (scene == nullptr) {
             return new ErrorCommand(cmd);
         } else {
             return new ShowCommand(scene);
         }
-    } else if (caseInsensitiveCompare(s1, "help")) {
+    } else if (caseInsensitiveCompare(words[0], "kill")) {
+        auto scene = getSceneById(std::atoi(words[1].c_str()));
+        if (scene == nullptr) {
+            return new ErrorCommand(cmd);
+        }
+        auto entity = scene->remove(std::atoi(words[2].c_str()));
+        if (entity == nullptr) {
+            return new ErrorCommand(cmd);
+        }
+        return new KillCommand(entity, &(Application::instance().getMap().getPlayer()));
+    } else if (caseInsensitiveCompare(words[0], "help")) {
         return new HelpCommand;
-    } else if (caseInsensitiveCompare(s1, "plant")) {
-        int s2;
-        iss >> s2;
-        auto scene = getSceneById(s2);
+    } else if (caseInsensitiveCompare(words[0], "plant")) {
+        auto scene = getSceneById(std::atoi(words[1].c_str()));
         if (scene == nullptr) {
             return new ErrorCommand(cmd);
         } else {
             return new PlantCommand(scene);
         }
+    } else if (caseInsensitiveCompare(words[0], "quit")) { 
+        return new QuitCommand;
+    } else if (caseInsensitiveCompare(words[0], "status")){
+        return new StatusCommand(&(Application::instance().getMap().getPlayer()));
     } else {
         return new ErrorCommand(cmd);
     }
@@ -105,4 +121,19 @@ void PlantCommand::execute(void) {
 void ErrorCommand::execute(void) {
     std::cout << "class:ErrorCommand method:execute\n";
     std::cout << "Error in executing command: " << cmd << '\n';
+}
+
+void KillCommand::execute(void) {
+    std::cout << "class:KillCommand method:execute\n";
+    if (entity == nullptr || player == nullptr) {
+        return;
+    }
+    player->acquire(entity->pick());
+}
+
+void StatusCommand::execute(void) {
+    std::cout << "class:StatusCommand method:execute\n";
+    if (player != nullptr) {
+        player->outputItems();
+    }
 }
